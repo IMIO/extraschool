d2c391ace4986ae40801b3605ed92c79feaccd0b
jmabe <jm.abe@labruyere.be>
2015-03-02 16:33:40 +0100

    correction smartphone

--- extraschool_smartphone.py
+++ extraschool_smartphone.py
@@ -23,6 +23,7 @@
 
 from openerp import models, api, fields
 from openerp.api import Environment
+from reportlab.graphics.barcode import createBarcodeImageInMemory
 import cStringIO
 import base64
 import os
@@ -49,16 +50,22 @@ class extraschool_smartphone(models.Model):
     oldversion = fields.Boolean('Old version')
     maxtimedelta = fields.Integer('Max time delta')
     
+    @api.multi
     def write(self,vals):
-        config = self.env['extraschool.mainsettings'].browse(1)
-        os.system(config.qrencode + ' -o ' + config.tempfolder + 'qrdownload.png -s 4 -l Q ' + self.softwareurl)        
-        qrfile = open(config.tempfolder + 'qrdownload.png','r').read()
-        vals['qrdownload'] = base64.b64encode(qrfile)
-        
-        self = super(extraschool_smartphone, self).write(vals)
-
-        os.system(config.qrencode + ' -o ' + config.tempfolder + 'qrconfig.png -s 4 -l Q "cfg;' + str(self.ids[0]) + ';' + self.transmissiontime + ';' + self.serveraddress + ';' + self.databasename + ';' + self.username + ';' + self.userpassword + ';' + self.scanmethod + ';' + self.transfertmethod + '"')        
-        qrfile = open(config['tempfolder']+'qrconfig.png','r').read()
-        vals['qrconfig'] = base64.b64encode(qrfile)
+        value='cfg;' + str(self.ids[0]) + ';' + self.transmissiontime + ';' + self.serveraddress + ';' + self.databasename + ';' + self.username + ';' + self.userpassword + ';' + self.scanmethod + ';' + self.transfertmethod
+        barcode = createBarcodeImageInMemory(
+                'QR', value=value, format='png', width=400, height=400,
+                humanReadable = 0
+            )        
+        vals['qrconfig'] = base64.b64encode(barcode)
+        #config = self.env['extraschool.mainsettings'].browse(1)
+        #os.system(config.qrencode + ' -o ' + config.tempfolder + 'qrdownload.png -s 4 -l Q ' + self.softwareurl)                
+        #qrfile = open(config.tempfolder + 'qrdownload.png','r').read()        
+        #vals['qrdownload'] = base64.b64encode(qrfile)        
+        self = super(extraschool_smartphone, self).write(vals)        
+        #os.system(config.qrencode + ' -o ' + config.tempfolder + 'qrconfig.png -s 4 -l Q "cfg;' + str(self.ids[0]) + ';' + self.transmissiontime + ';' + self.serveraddress + ';' + self.databasename + ';' + self.username + ';' + self.userpassword + ';' + self.scanmethod + ';' + self.transfertmethod + '"')                
+        #qrfile = open(config['tempfolder']+'qrconfig.png','r').read()        
+        #vals['qrconfig'] = base64.b64encode(qrfile)
+        return self
         
 extraschool_smartphone()
