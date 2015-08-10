506c623f4659d8741d345090873a66f6a92e4627
Michael Michot <michael.michot@imio.be>
2015-08-10 15:30:47 +0200

    Hack odoo fct to support more paper format specific args

--- extraschool_report_hack.py
+++ extraschool_report_hack.py
@@ -27,13 +27,35 @@ class extraschool_report_hack(osv.Model):
     _inherit = 'report'
     
     def _build_wkhtmltopdf_args(self, paperformat, specific_paperformat_args=None):
-        print "!!!!!!!!!!!!!!!Report Hack!!!!!!!!!!!!!!!!!!!"
         """Hack the parent fct to remove dpi arg from the return value."""
         res = super(extraschool_report_hack,self)._build_wkhtmltopdf_args(paperformat, specific_paperformat_args)
         if '--dpi' in res:
             i = res.index('--dpi')
             del res[i] #delete --dpi key
             del res[i] #delete --dpi value
-            
-#        pdb.set_trace()
+        
+        if specific_paperformat_args and specific_paperformat_args.get('data-report-margin-bottom'):
+            if "--margin-bottom" in res:
+                res[res.index('--margin-bottom') + 1] =  str(specific_paperformat_args.get('data-report-margin-bottom')) + 'mm'
+            else:
+                res.extend(['--margin-bottom', str(specific_paperformat_args.get('data-report-margin-bottom')) + 'mm'])
+
+        if specific_paperformat_args and specific_paperformat_args.get('data-report-margin-top'):
+            if "--margin-top" in res:
+                res[res.index('--margin-top') + 1] =  str(specific_paperformat_args.get('data-report-margin-top')) + 'mm'
+            else:
+                res.extend(['--margin-top', str(specific_paperformat_args.get('data-report-margin-top')) + 'mm'])
+        
+        if specific_paperformat_args and specific_paperformat_args.get('data-report-margin-right'):
+            if "--margin-right" in res:
+                res[res.index('--margin-right') + 1] = str(specific_paperformat_args.get('data-report-margin-right')) + 'mm'
+            else:
+                res.extend(['--margin-right', str(specific_paperformat_args.get('data-report-margin-right')) + 'mm'])
+        
+        if specific_paperformat_args and specific_paperformat_args.get('data-report-margin-left'):
+            if "--margin-left" in res:
+                res[res.index('--margin-left') + 1] = str(specific_paperformat_args.get('data-report-margin-left')) + 'mm'
+            else:
+                res.extend(['--margin-left', str(specific_paperformat_args.get('data-report-margin-left')) + 'mm'])
+ 
         return res
