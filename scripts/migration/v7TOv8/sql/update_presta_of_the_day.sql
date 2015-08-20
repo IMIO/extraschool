8a7bed5362614a0b697b2cd83f166201d6b8ba72
Michael Michot <michael.michot@imio.be>
2015-08-20 09:54:38 +0200

    add insert presta of the day

--- scripts/migration/v7TOv8/sql/update_presta_of_the_day.sql
+++ scripts/migration/v7TOv8/sql/update_presta_of_the_day.sql
@@ -1,3 +1,9 @@
+insert into extraschool_prestation_times_of_the_day
+(date_of_the_day, child_id, verified)
+select prestation_date, childid, False as verified
+from extraschool_pdaprestationtimes
+group by childid, prestation_date, verified;
+
 update extraschool_prestationtimes 
 	set prestation_times_of_the_day_id = (select id from extraschool_prestation_times_of_the_day where child_id = childid and date_of_the_day = prestation_date);
 update extraschool_pdaprestationtimes 
