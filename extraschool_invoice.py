abf90af895c5ff6872f4042e6e32bdc4570e142d
Michael Michot <michael.michot@imio.be>
2015-08-26 13:11:29 +0200

    amount compute fct

--- extraschool_invoice.py
+++ extraschool_invoice.py
@@ -34,8 +34,8 @@ class extraschool_invoice(models.Model):
     number = fields.Integer('Number',readonly=True)
     structcom = fields.Char('Structured Communication', size=50,readonly=True, index = True)
     amount_total = fields.Float('Amount',readonly=True)
-    amount_received = fields.Float('Received',readonly=True)
-    balance = fields.Float('Balance',readonly=True)
+    amount_received = fields.Float(compute="_compute_amount_received", string='Received',readonly=True,store=True)
+    balance = fields.Float(compute="_compute_balance",string='Balance',readonly=True, store=True)
     no_value = fields.Float('No value',readonly=True)
     discount = fields.Float('Discount',readonly=True)
     biller_id = fields.Many2one('extraschool.biller', 'Biller', required=False,ondelete='cascade',readonly=True)
@@ -47,5 +47,16 @@ class extraschool_invoice(models.Model):
     activitycategoryid = fields.Many2one(related='biller_id.activitycategoryid')
     period_from = fields.Date(related='biller_id.period_from')
     period_to = fields.Date(related='biller_id.period_to')
-    payment_term = fields.Date(related='biller_id.payment_term')      
+    payment_term = fields.Date(related='biller_id.payment_term')  
+        
 
+    @api.onchange('payment_ids')
+    @api.depends('payment_ids')
+    def _compute_amount_received(self):
+        for invoice in self:
+            invoice.amount_received = sum(reconcil_line.amount for reconcil_line in invoice.payment_ids)
+
+    @api.depends('amount_received')
+    def _compute_balance(self):
+        for invoice in self:
+            invoice.balance = invoice.amount_total - invoice.amount_received
