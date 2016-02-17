8323d601a59c2cd057c434a93c3e64007d668535
Michael Michot <michael.michot@imio.be>
2016-02-17 12:23:19 +0100

    version 1.1.5 correction process verif presta

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
 
 {
     'name' : 'Extraschool',
-    'version' : '1.1.4',
+    'version' : '1.1.5',
     'author' : 'Town of La Bruyère and Imio',
     'depends' : ['base', 
                  'report',
