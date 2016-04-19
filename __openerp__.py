0af15ac2f2e56d26f8cc461f103bb68b8ce4da20
Michael Michot <michael.michot@imio.be>
2016-04-19 20:22:30 +0200

    v 1.2.9 order taxcertif on implantation,class,parent

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
 
 {
     'name' : 'Extraschool',
-    'version' : '1.2.8',
+    'version' : '1.2.9',
     'author' : 'Town of La Bruyère and Imio',
     'depends' : ['base', 
                  'report',
