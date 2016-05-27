709abe944da28067426864c940a36eea9dfaa7c7
Michael Michot <michael.michot@imio.be>
2016-05-27 15:48:50 +0200

    version 1.3.3 reminder v0.9 + import child with RN (seraing)

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
 
 {
     'name' : 'Extraschool',
-    'version' : '1.3.2',
+    'version' : '1.3.3',
     'author' : 'Town of La Bruyère and Imio',
     'depends' : ['base', 
                  'report',
