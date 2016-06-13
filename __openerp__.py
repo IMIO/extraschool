3a821070dbb9d3503ebe3aa51fc18e0e4a814c99
Michael Michot <michael.michot@imio.be>
2016-06-13 22:03:12 +0200

    version 1.3.5

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
 
 {
     'name' : 'Extraschool',
-    'version' : '1.3.4',
+    'version' : '1.3.5',
     'author' : 'Town of La Bruyère and Imio',
     'depends' : ['base', 
                  'report',
