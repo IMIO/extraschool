3d323171df720921163ba17e292a8f1da53d48f7
Michael Michot <michael.michot@imio.be>
2016-09-07 12:00:58 +0200

    ajout biller ref on report reconcil payment

--- extraschool_payment.py
+++ extraschool_payment.py
@@ -105,6 +105,7 @@ class extraschool_payment_reconciliation(models.Model):
     payment_id = fields.Many2one("extraschool.payment", required=True,ondelete='cascade')
     invoice_id = fields.Many2one("extraschool.invoice", required=True)
     biller_id = fields.Many2one(related='invoice_id.biller_id', store=True)
+    biller_other_ref = fields.Char(related='invoice_id.biller_id.other_ref', store=True)
     amount = fields.Float('Amount')
     account = fields.Char(related='payment_id.account')
     paymentdate = fields.Date(related='payment_id.paymentdate', store=True)
