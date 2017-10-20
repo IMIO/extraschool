890328075d159276b3ef73d65499f49b46a52d59
Mickey <michael.colicchia@imio.be>
2017-10-20 11:23:24 +0200

    New query to get commstruct of parents

---
+++ scripts/select_comstruct_parent.sql
@@ -0,0 +1,6 @@
+﻿SELECT DISTINCT(pay.parent_id), par.lastname AS Nom, par.firstname AS Prénom, pay.structcom AS Communication_structurée
+FROM extraschool_payment AS pay
+INNER JOIN extraschool_parent as par
+ON pay.parent_id = par.id
+WHERE pay.structcom IS NOT NULL
+ORDER BY par.lastname,par.firstname;
