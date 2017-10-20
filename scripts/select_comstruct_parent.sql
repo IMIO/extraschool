f94651bf492decf383aec01cfc62293a8a5d07d6
Mickey <michael.colicchia@imio.be>
2017-10-20 13:50:05 +0200

    Added address of parent to the query

--- scripts/select_comstruct_parent.sql
+++ scripts/select_comstruct_parent.sql
@@ -1,4 +1,4 @@
-﻿SELECT DISTINCT(pay.parent_id), par.lastname AS Nom, par.firstname AS Prénom, pay.structcom AS Communication_structurée
+﻿SELECT DISTINCT(pay.parent_id), par.lastname AS Nom, par.firstname AS Prénom, par.street AS Rue, par.zipcode AS Code_Postal, par.city AS Ville, pay.structcom AS Communication_structurée
 FROM extraschool_payment AS pay
 INNER JOIN extraschool_parent as par
 ON pay.parent_id = par.id
