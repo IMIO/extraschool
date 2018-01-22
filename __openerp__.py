7e20a26ee04d861139106aa6f53ca77404083bcc
Mickey <michael.colicchia@imio.be>
2018-01-22 14:34:48 +0100

    Correction on parent's comstruct

--- __openerp__.py
+++ __openerp__.py
@@ -37,6 +37,7 @@
     'test' : [],
     'data' : [
         'scripts/update_seq.xml',
+        'scripts/update_comstruct.xml',
 
         'views/extraschool_activitycategory.xml',
         'views/extraschool_activity.xml',
