198363d8f476c1eeb2a133275e72228b2d6bb212
mcolicchia <michael.colicchia@imio.be>
2018-05-07 11:47:56 +0200

    9.006

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
 
 {
     'name' : 'Extraschool',
-    'version' : '9.0.0.0.5',
+    'version' : '9.0.0.0.6',
     'author' : 'Town of La Bruyère and Imio',
     'depends' : ['base',
                  'report',
