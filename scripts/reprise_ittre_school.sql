0d1093d002073b45c9ed4d53509bea78e2725d01
mcolicchia <michael.colicchia@imio.be>
2018-06-19 16:16:17 +0200

    add sql script

---
+++ scripts/reprise_ittre_school.sql
@@ -0,0 +1,47 @@
+﻿UPDATE extraschool_child_registration
+SET place_id = 6
+WHERE place_id = 1;
+
+UPDATE extraschool_pdaprestationtimes
+SET placeid = 6
+WHERE placeid = 1;
+
+UPDATE extraschool_prestation_times_encodage_manuel
+SET place_id = 6
+WHERE place_id = 1;
+
+UPDATE extraschool_child_registration
+SET place_id = 9
+WHERE place_id = 4;
+
+UPDATE extraschool_pdaprestationtimes
+SET placeid = 9
+WHERE placeid = 4;
+
+UPDATE extraschool_prestation_times_encodage_manuel
+SET place_id = 9
+WHERE place_id = 4;
+
+UPDATE extraschool_child_registration
+SET place_id = 7
+WHERE place_id = 2;
+
+UPDATE extraschool_pdaprestationtimes
+SET placeid = 7
+WHERE placeid = 2;
+
+UPDATE extraschool_prestation_times_encodage_manuel
+SET place_id = 7
+WHERE place_id = 2;
+
+UPDATE extraschool_child_registration
+SET place_id = 8
+WHERE place_id = 3;
+
+UPDATE extraschool_pdaprestationtimes
+SET placeid = 8
+WHERE placeid = 3;
+
+UPDATE extraschool_prestation_times_encodage_manuel
+SET place_id = 8
+WHERE place_id = 3;
