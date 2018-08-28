66ca6371df19b1ea109732681d11670a95edf24d
mcolicchia <m.colicchia@imio.be>
2018-08-28 08:45:52 +0000

    [FIX] payment_type is Integer

--- extraschool_payment.py
+++ extraschool_payment.py
@@ -86,7 +86,7 @@ class extraschool_payment(models.Model):
         # On CODA payment, do not pay tagged or reminder/reminder fees invoice.
         if from_coda:
             search_domain += [('tag', '=', None), ('last_reminder_id', '=', None)]
-        elif payment_type == '1':  # Prepaid.
+        elif payment_type == 1:  # Prepaid.
             activity_category_ids = self.env['extraschool.activitycategory'].search([('payment_invitation_com_struct_prefix', '=', com_struct_prefix)]).ids
             search_domain += [('activitycategoryid', 'in',activity_category_ids),]
 
