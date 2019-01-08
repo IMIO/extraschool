50d20daf5a9321e8a14bcd582c8177946a518431
mcolicchia <michael.colicchia@imio.be>
2019-01-08 11:39:15 +0100

    [ADD] rework model and view

--- extraschool_organising_power.py
+++ extraschool_organising_power.py
@@ -32,3 +32,49 @@ class extraschool_organising_power(models.Model):
     max_school_implantation = fields.Integer()
     dominant_activity_category_id = fields.Many2one('extraschool.activitycategory')
 
+    po_name = fields.Char('Name of PO')
+    po_street = fields.Char('Street')
+    po_zipcode = fields.Char('ZipCode')
+    po_city = fields.Char('City')
+    po_sign = fields.Binary('Signature')
+
+    po_attestation_name = fields.Char('Name of resp tax certificate')
+    po_attestation_fct = fields.Char('Fct of resp tax certificate')
+    po_attestation_sign = fields.Binary('Signature of resp tax certificate')
+
+    po_email = fields.Char('email')
+    po_tel = fields.Char('tel')
+    po_addresse_free_text = fields.Char('Adresse texte libre')
+    po_addresse_free_text2 = fields.Char('Adresse texte libre 2')
+
+    po_stamp = fields.Binary('stamp')
+    po_sign_img = fields.Binary('Signature image')
+
+    po_resp_name = fields.Char('Name of resp')
+    po_resp_fct = fields.Char('Fct of resp')
+    po_resp2_sign = fields.Binary('Signature of resp2')
+    po_resp2_name = fields.Char('Name of resp2')
+    po_resp2_fct = fields.Char('Fct of resp2')
+
+    po_rappel_name = fields.Char('Name of resp reminder')
+    po_rappel_fct = fields.Char('Fct of resp reminder')
+    po_rappel_sign = fields.Binary('Signature of resp reminder')
+
+    def reprise_signaletic(self):
+        activity_category = self.env['extraschool.activitycategory'].search([])[0]
+
+        self.po_name = activity_category.po_addresse_free_text
+        self.po_street =
+        self.po_email =
+        self.po_zipcode =
+        self.po_city =
+        self.po_city =
+        self.po_sign =
+        self.po_stamp =
+        self.po_tel =
+        self.po_addresse_free_text =
+        self.po_addresse_free_text2 =
+        self.po_resp_name =
+        self.po_resp_fct =
+        self
+
