43cbf1567dc2a0810258bd460c203655c4914788
mcolicchia <michael.colicchia@imio.be>
2019-02-12 10:41:46 +0100

    [ADD] change in api

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
 
 {
     'name' : 'Extraschool',
-    'version' : '9.0.0.0.8',
+    'version' : '9.0.0.0.9',
     'author' : 'Town of La Bruyère and Imio',
     'depends' : ['base',
                  'report',
