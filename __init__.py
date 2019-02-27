9dd615e6efffbb9c16bf7aff1938ac41f7693d40
coco <coralie.cardon@imio.be>
2019-02-27 11:32:10 +0100

    rapport

--- __init__.py
+++ __init__.py
@@ -59,6 +59,7 @@ import extraschool_prestation_times_encodage_manuel_wizard
 import extraschool_presta_stat
 import extraschool_presta_activity
 import extraschool_subvention_report
+import extraschool_plaine_report
 import extraschool_price_list
 import extraschool_qrcodes_wizard
 import extraschool_refound_line
---
+++ extraschool_plaine_report.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Extraschool
+#    Copyright (C) 2008-2014
+#    Jean-Michel Abé - Town of La Bruyère (<http://www.labruyere.be>)
+#    Michael Michot & Michael Colicchial - Imio (<http://www.imio.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from docutils.utils.math.math2html import Formula
+
+from openerp import models, api, fields,_
+import base64
+import os
+import datetime
+import xlsxwriter
+from datetime import date, datetime, timedelta as td
+from openerp.exceptions import except_orm, Warning, RedirectWarning
+
+
+
+class extraschool_plaine_report(models.Model):
+    _name = 'extraschool.plaine_report'
+
+    name = fields.Char('Nom', required=True)
+    validity_from = fields.Date('Date from')
+    validity_to = fields.Date('Date to')
+
+    @api.multi
+    def generate_plaine(self):
+
+
+        if self.validity_from > self.validity_to:
+            raise Warning(_("La date de fin doit être plus grande que la date de début !"))
+
+        report = self.env['report']._get_report_from_name('extraschool.tpl_plaine_report_wizard_report')
+
+        datas = {
+            'ids': self.ids,
+            'model': report.model,
+        }
+        print "ok"
+
+        return {
+            'type': 'ir.actions.report.xml',
+            'report_name': 'extraschool.tpl_plaine_report_wizard_report',
+            'datas': datas,
+            'report_type': 'qweb-pdf',
+            'target': 'new'
+        }
+
+
+        '''report = '/tmp/plaine_report' + str(datetime.now()) + '.xls'
+        workbook = xlsxwriter.Workbook(report)
+
+        title = workbook.add_format({'bold': True, 'bg_color': '#cccccc', 'font_size': 20, 'border': True})
+        bold = workbook.add_format({'bold': True, 'border': True})
+        border = workbook.add_format({'border': True})
+
+        # - de 6 ans
+        worksheet = workbook.add_worksheet('- 6 ans')
+        worksheet.merge_range('A1:E1', "ENFANTS DE - DE 6 ANS", title)
+        worksheet.write(1,0, "Nom", bold)
+        worksheet.write(1,1, u"Prénom", bold)
+        worksheet.write(1,2, "Age", bold)
+        worksheet.write(1,3, "Nombre de jours", bold)
+        worksheet.write(1,4, "Prix", bold)
+
+        row = 2
+        total_prestation=total_price = 0
+
+        for child in under_6:
+            worksheet.write(row, 0, under_6[child]['lastname'], border)
+            worksheet.write(row, 1, under_6[child]['firstname'], border)
+            worksheet.write(row, 2, str(under_6[child]['age']) + " ans", border)
+            worksheet.write(row, 3, under_6[child]['prestation'], border)
+            worksheet.write(row, 4, str(under_6[child]['price']) + "€".decode('utf-8'), border)
+            row += 1
+            total_prestation += under_6[child]['prestation']
+            total_price += under_6[child]['price']
+        worksheet.merge_range('A' + str(row +1) + ':C' + str(row +1), "Total", bold)
+        worksheet.write(row, 3, total_prestation, border)
+        worksheet.write(row, 4, str(total_price) + "€".decode('utf-8'), border)
+
+        # + de 6 ans
+        worksheet = workbook.add_worksheet('+ 6 ans')
+        worksheet.merge_range('A1:E1', "ENFANTS DE + DE 6 ANS", title)
+        worksheet.write(1,0, "Nom", bold)
+        worksheet.write(1,1, u"Prénom", bold)
+        worksheet.write(1,2, "Age", bold)
+        worksheet.write(1,3, "Nombre de jours", bold)
+        worksheet.write(1,4, "Prix", bold)
+
+        row = 2
+        total_prestation = total_price = 0
+
+        for child in over_6:
+            worksheet.write(row, 0, over_6[child]['lastname'], border)
+            worksheet.write(row, 1, over_6[child]['firstname'], border)
+            worksheet.write(row, 2, str(over_6[child]['age']) + " ans", border)
+            worksheet.write(row, 3, over_6[child]['prestation'], border)
+            worksheet.write(row, 4, str(over_6[child]['price']) + "€".decode('utf-8'), border)
+            row += 1
+            total_prestation += over_6[child]['prestation']
+            total_price += over_6[child]['price']
+        worksheet.merge_range('A' + str(row + 1) + ':C' + str(row + 1), "Total", bold)
+        worksheet.write(row, 3, total_prestation, border)
+        worksheet.write(row, 4, str(total_price) + "€".decode('utf-8'), border)
+
+        workbook.close()
+
+        outfile = open(report, "r").read()
+        attachment_obj = self.env['ir.attachment']
+        attachment_obj.create({'res_model': 'extraschool.plaine_report', 'res_id': id.id,
+                               'datas_fname': 'rapport_plaine_' + '.xls',
+                               'name': 'rapport_one_' + '.xls',
+                               'datas': base64.b64encode(outfile), })
+        os.remove(report)
+
+    @api.model
+    def create(self,vals):
+
+        under_6 = {}
+        over_6 = {}
+
+        prestation_ids = self.env['extraschool.prestationtimes'].search([
+            ('prestation_date', '>=', vals.get('start_date')),
+            ('prestation_date', '<=', vals.get('end_date')),
+            ('activity_occurrence_id.activityid', 'in', tuple(vals.get('activity')[0][2])),
+            ('es', '=', 'E'),
+        ]).sorted(key=lambda r: (r.childid.lastname))
+
+        for prestation in prestation_ids :
+            age = prestation.childid.get_age()
+            if age >= 6 :
+                if prestation.childid.id in over_6:
+                    over_6[prestation.childid.id]['prestation'] += 1
+                    over_6[prestation.childid.id]['price'] += prestation.invoiced_prestation_id.total_price
+                else:
+                    over_6[prestation.childid.id] = {'lastname': prestation.childid.lastname.upper(),
+                                                     'firstname': prestation.childid.firstname, 'age': age,
+                                                     'prestation': 1,
+                                                     'price': prestation.invoiced_prestation_id.total_price}
+            else:
+                if prestation.childid.id in under_6:
+                    under_6[prestation.childid.id]['prestation'] += 1
+                    under_6[prestation.childid.id]['price'] += prestation.invoiced_prestation_id.total_price
+                else:
+                    under_6[prestation.childid.id] = {'lastname': prestation.childid.lastname.upper(),
+                                                      'firstname': prestation.childid.firstname, 'age': age,
+                                                      'prestation': 1,
+                                                      'price': prestation.invoiced_prestation_id.total_price}
+
+        id = super(extraschool_plaine_report, self).create(vals)
+        self.generate_report(under_6,over_6,id)
+
+        return id'''
