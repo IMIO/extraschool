816394f54d3b60daaa059b40ca25e1ea8a42c611
Michaël Colicchia <m.colicchia@imio.be>
2019-09-03 12:56:30 +0200

    [AES-135] put complexity/computing in models, not in view

--- models/extraschool_parent.py
+++ models/extraschool_parent.py
@@ -34,65 +34,69 @@ import random
 import urllib
 import urlparse
 
+
 class extraschool_parent(models.Model):
     _name = 'extraschool.parent'
     _description = 'Parent'
     _inherit = 'mail.thread'
     _order = 'lastname'
 
-    @api.depends('firstname','lastname')
+    @api.depends('firstname', 'lastname')
     def _name_compute(self):
         for record in self:
-            record.name = '%s %s'  % (record.lastname, record.firstname)
+            record.name = '%s %s' % (record.lastname, record.firstname)
 
     def _search_fullname(self, operator, value):
-        return ['|',('firstname', operator, value),('lastname', operator, value)]
+        return ['|', ('firstname', operator, value), ('lastname', operator, value)]
 
-    def onchange_address(self, cr, uid, ids, street,city):
-        v={}
+    def onchange_address(self, cr, uid, ids, street, city):
+        v = {}
         if street:
             if city:
-                v['streetcode']= lbutils.genstreetcode(street + city)
+                v['streetcode'] = lbutils.genstreetcode(street + city)
             else:
-                v['streetcode']= lbutils.genstreetcode(street)
-        return {'value':v}
+                v['streetcode'] = lbutils.genstreetcode(street)
+        return {'value': v}
 
     def return_action_to_open(self, cr, uid, ids, context=None):
         """ This opens the xml view specified in xml_id for the current parent """
         if context is None:
             context = {}
         if context.get('xml_id'):
-            res = self.pool.get('ir.actions.act_window').for_xml_id(cr, uid ,'extraschool', context['xml_id'], context=context)
+            res = self.pool.get('ir.actions.act_window').for_xml_id(cr, uid, 'extraschool', context['xml_id'],
+                                                                    context=context)
             res['context'] = context
-            res['domain'] = [('parentid','=', ids[0])]
+            res['domain'] = [('parentid', '=', ids[0])]
             return res
         return False
 
-    def _compute_totalinvoiced (self):
+    def _compute_totalinvoiced(self):
         cr = self.env.cr
         for record in self:
-            cr.execute('select sum(amount_total) from extraschool_invoice where parentid=%s',(record.id,))
+            cr.execute('select sum(amount_total) from extraschool_invoice where parentid=%s', (record.id,))
             record.totalinvoiced = cr.fetchall()[0][0]
 
-
-    def _compute_totalreceived (self):
+    def _compute_totalreceived(self):
         cr = self.env.cr
         for record in self:
-            cr.execute('select sum(amount_received) from extraschool_invoice where parentid=%s',(record.id,))
+            cr.execute('select sum(amount_received) from extraschool_invoice where parentid=%s', (record.id,))
             record.totalreceived = cr.fetchall()[0][0]
 
-    def _compute_totalbalance (self):
+    def _compute_totalbalance(self):
         cr = self.env.cr
         for record in self:
-            cr.execute('select sum(balance) from extraschool_invoice where parentid=%s and (huissier = False or huissier is Null)',(record.id,))
+            cr.execute(
+                'select sum(balance) from extraschool_invoice where parentid=%s and (huissier = False or huissier is Null)',
+                (record.id,))
             invoice = cr.fetchall()[0][0]
             invoice = invoice if invoice else 0.0
             record.totalbalance = invoice
 
-    def _compute_totalhuissier (self):
+    def _compute_totalhuissier(self):
         cr = self.env.cr
         for record in self:
-            cr.execute('select sum(balance) from extraschool_invoice where parentid=%s and huissier = True',(record.id,))
+            cr.execute('select sum(balance) from extraschool_invoice where parentid=%s and huissier = True',
+                       (record.id,))
             record.totalhuissier = cr.fetchall()[0][0]
 
     # def _compute_total_reminder_fees (self):
@@ -101,7 +105,7 @@ class extraschool_parent(models.Model):
     #         cr.execute('select sum(fees_amount) from extraschool_reminder where parentid=%s',(record.id,))
     #         record.total_reminder_fees = cr.fetchall()[0][0]
 
-    name = fields.Char(compute='_name_compute',string='FullName', search='_search_fullname', size=100)
+    name = fields.Char(compute='_name_compute', string='FullName', search='_search_fullname', size=100)
     rn = fields.Char('RN', track_visibility='onchange')
     firstname = fields.Char('FirstName', size=50, required=True, track_visibility='onchange')
     lastname = fields.Char('LastName', size=50, required=True, track_visibility='onchange')
@@ -112,33 +116,36 @@ class extraschool_parent(models.Model):
     workphone = fields.Char('Work Phone', size=20, track_visibility='onchange')
     gsm = fields.Char('GSM', size=20, track_visibility='onchange')
     email = fields.Char('Email', size=100, track_visibility='onchange')
-    invoicesendmethod = fields.Selection((('emailandmail','By mail and email'),
-                                          ('onlyemail','Only by email'),
-                                          ('onlybymail','Only by mail')),
-                                         'Invoice send method',required=True, default='onlybymail', track_visibility='onchange')
+    invoicesendmethod = fields.Selection((('emailandmail', 'By mail and email'),
+                                          ('onlyemail', 'Only by email'),
+                                          ('onlybymail', 'Only by mail')),
+                                         'Invoice send method', required=True, default='onlybymail',
+                                         track_visibility='onchange')
     streetcode = fields.Char('Street code', size=50, track_visibility='onchange')
-    child_ids = fields.One2many('extraschool.child', 'parentid','childs', readonly=True)
-    invoice_ids = fields.One2many('extraschool.invoice', 'parentid','invoices', track_visibility='onchange')
-    remindersendmethod = fields.Selection((('emailandmail','By mail and email'),
-                                           ('onlyemail','Only by email'),
-                                           ('onlybymail','Only by mail')),
-                                          'Reminder send method',required=True, default='onlybymail', track_visibility='onchange')
-    one_subvention_type = fields.Selection((('sf','operating grants'),
-                                            ('sdp','positive differentiation grants')),
+    child_ids = fields.One2many('extraschool.child', 'parentid', 'childs', readonly=True)
+    invoice_ids = fields.One2many('extraschool.invoice', 'parentid', 'invoices', track_visibility='onchange')
+    remindersendmethod = fields.Selection((('emailandmail', 'By mail and email'),
+                                           ('onlyemail', 'Only by email'),
+                                           ('onlybymail', 'Only by mail')),
+                                          'Reminder send method', required=True, default='onlybymail',
+                                          track_visibility='onchange')
+    one_subvention_type = fields.Selection((('sf', 'operating grants'),
+                                            ('sdp', 'positive differentiation grants')),
                                            required=True, default='sf', track_visibility='onchange')
-    reminder_ids = fields.One2many('extraschool.reminder', 'parentid','reminders', readonly=True)
+    reminder_ids = fields.One2many('extraschool.reminder', 'parentid', 'reminders', readonly=True)
     totalinvoiced = fields.Float(compute='_compute_totalinvoiced', string="Total invoiced")
     totalreceived = fields.Float(compute='_compute_totalreceived', string="Total received")
     totalbalance = fields.Float(compute='_compute_totalbalance', string="Total balance")
     # total_reminder_fees = fields.Float(compute='_compute_total_reminder_fees', string="Total reminder fees")
     totalhuissier = fields.Float(compute='_compute_totalhuissier', string="Total huissier", track_visibility='onchange')
-    payment_ids = fields.One2many('extraschool.payment','parent_id')
-    payment_status_ids = fields.One2many('extraschool.payment_status_report','parent_id')
+    payment_ids = fields.One2many('extraschool.payment', 'parent_id')
+    payment_status_ids = fields.One2many('extraschool.payment_status_report', 'parent_id')
     last_import_date = fields.Datetime('Import date', readonly=True, track_visibility='onchange')
     modified_since_last_import = fields.Boolean('Modified since last import')
     isdisabled = fields.Boolean('Disabled', track_visibility='onchange')
     oldid = fields.Integer('oldid')
-    nbr_actif_child = fields.Integer(compute='_compute_nbr_actif_child',string='Nbr actif child', store = True, track_visibility='onchange')
+    nbr_actif_child = fields.Integer(compute='_compute_nbr_actif_child', string='Nbr actif child', store=True,
+                                     track_visibility='onchange')
     comment = fields.Text('Comment', track_visibility='onchange')
     comstruct = fields.Char('Structured Communication', readonly=True)
     country_id = fields.Many2one('res.country', string='Country', default=21, required=True)
@@ -153,13 +160,13 @@ class extraschool_parent(models.Model):
         else:
             self.check_name = True
 
-        v={}
+        v = {}
         if self.lastname:
             if self.firstname:
-                v['name']='%s %s' % (self.lastname, self.firstname)
+                v['name'] = '%s %s' % (self.lastname, self.firstname)
             else:
-                v['name']=self.lastname
-        return {'value':v}
+                v['name'] = self.lastname
+        return {'value': v}
 
     @api.onchange('rn')
     @api.multi
@@ -179,8 +186,8 @@ class extraschool_parent(models.Model):
                 'nodestroy': False,
                 'target': 'current',
                 'limit': 50000,
-                'domain': [('parentid', '=',self.id),]
-            }
+                'domain': [('parentid', '=', self.id), ]
+                }
 
     @api.multi
     def get_tax_certificate(self):
@@ -192,8 +199,8 @@ class extraschool_parent(models.Model):
                 'nodestroy': False,
                 'target': 'current',
                 'limit': 50000,
-                'domain': [('parent_id', '=',self.id),]
-            }
+                'domain': [('parent_id', '=', self.id), ]
+                }
 
     @api.multi
     def get_payment(self):
@@ -210,7 +217,7 @@ class extraschool_parent(models.Model):
                 'domain': [('payment_id.parent_id', '=', self.id), ('amount', '>', 0.0001)]
                 }
 
-    def email_validation(self,email):
+    def email_validation(self, email):
         if re.match("^[a-zA-Z0-9.+_%-]+@[a-zA-Z0-9._%-]+.[a-zA-Z]{2,6}$", email) != None:
             return True
         else:
@@ -238,19 +245,20 @@ class extraschool_parent(models.Model):
     def create(self, vals):
         # todo replace check par une contraite
         parent_obj = self.env['extraschool.parent']
-        parents=parent_obj.search([('firstname', 'ilike', vals['firstname'].strip()),
-                                   ('lastname', 'ilike', vals['lastname'].strip()),
-                                   ])
+        parents = parent_obj.search([('firstname', 'ilike', vals['firstname'].strip()),
+                                     ('lastname', 'ilike', vals['lastname'].strip()),
+                                     ])
 
         if vals['email'] != False and vals['email'] != '' and vals['email'] != ' ':
             emails = vals['email'].split(',')
-            for email in emails :
+            for email in emails:
                 if (not self.email_validation(email)):
                     raise Warning("E-mail format invalid.")
 
         # Compute and store parent's commstruct for further use (search).
         parent_id = super(extraschool_parent, self).create(vals)
-        parent_id.write({'comstruct': parent_id.get_prepaid_comstruct(self.env['extraschool.activitycategory'].search([])[0])})
+        parent_id.write(
+            {'comstruct': parent_id.get_prepaid_comstruct(self.env['extraschool.activitycategory'].search([])[0])})
 
         return parent_id
 
@@ -277,7 +285,7 @@ class extraschool_parent(models.Model):
             'nodestroy': True,
             'target': 'new',
             'domain': '[]',
-            'context': {'parent_id' : ids[0]}
+            'context': {'parent_id': ids[0]}
         }
 
     @api.multi
@@ -298,16 +306,16 @@ class extraschool_parent(models.Model):
                               'email',
                               ])
 
-        if fields_to_find.intersection(set([k for k,v in vals.iteritems()])):
+        if fields_to_find.intersection(set([k for k, v in vals.iteritems()])):
             vals['modified_since_last_import'] = True
 
         if 'email' in vals and vals['email'] != False:
             emails = vals['email'].split(',')
-            for email in emails :
+            for email in emails:
                 if (not self.email_validation(email)):
                     raise Warning("E-mail format invalid.")
 
-        return super(extraschool_parent,self).write(vals)
+        return super(extraschool_parent, self).write(vals)
 
     @api.one
     def unlink(self):
@@ -316,8 +324,19 @@ class extraschool_parent(models.Model):
     def get_prepaid_comstruct(self, categ):
         com_struct_prefix_str = categ.payment_invitation_com_struct_prefix
         com_struct_id_str = str(self.id).zfill(7)
-        com_struct_check_str = str(long(com_struct_prefix_str+com_struct_id_str) % 97).zfill(2)
+        com_struct_check_str = str(long(com_struct_prefix_str + com_struct_id_str) % 97).zfill(2)
         com_struct_check_str = com_struct_check_str if com_struct_check_str != '00' else '97'
 
+        return self.env['extraschool.payment'].format_comstruct(
+            '%s%s%s' % (com_struct_prefix_str, com_struct_id_str, com_struct_check_str))
 
-        return self.env['extraschool.payment'].format_comstruct('%s%s%s' % (com_struct_prefix_str,com_struct_id_str,com_struct_check_str))
+    @api.multi
+    def get_all_invoice_activity_category(self, invoice_id):
+        activity_category_id = self.env['extraschool.invoice'].browse(invoice_id).activitycategoryid
+        return self.invoice_ids.filtered(lambda r: r.activitycategoryid == activity_category_id)
+
+    @api.multi
+    def get_sum_invoice_by_activity_category(self, invoice_id):
+        activity_category_id = self.env['extraschool.invoice'].browse(invoice_id).activitycategoryid
+        sum_invoice = sum(x.balance for x in self.invoice_ids.filtered(lambda r: r.activitycategoryid == activity_category_id))
+        return sum_invoice
