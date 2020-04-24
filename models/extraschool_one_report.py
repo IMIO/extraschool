81ab74124b0cd9edb1ae9da8db25533d3e96eb19
Jenny Pans <jenny.pans@imio.be>
2020-04-24 10:48:36 +0200

    [AES-295][FIX] Fix durbuy and Hastiere because of synthesis in one report. Also add translations

--- models/extraschool_one_report.py
+++ models/extraschool_one_report.py
@@ -90,6 +90,15 @@ class extraschool_one_report(models.Model):
     synthesis = fields.Boolean()
     not_created = fields.Boolean(default=True)
 
+    @api.model
+    def update_place_ids(self):
+        one_report_ids = self.env['extraschool.one_report'].search([])
+        for one_report in one_report_ids:
+            if one_report.placeid:
+                one_report.write({
+                    'place_ids': [(6, 0, [one_report.placeid.id])]
+                })
+
     @api.onchange('place_ids', 'year', 'quarter')
     def _is_created(self):
         """
