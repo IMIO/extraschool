eed171c7eb6db6f3f9ddc21a483a5e5c4e1433b7
jpans <jenny.pans@imio.be>
2020-09-17 12:04:58 +0200

    remove wdb

--- models/extraschool_coda.py
+++ models/extraschool_coda.py
@@ -89,8 +89,6 @@ class extraschool_coda(models.Model):
 
     @api.model
     def create(self, vals):
-        import wdb
-        wdb.set_trace()
         self._ensure_coda_file_exists(vals)
         #to do refactoring suite api V8
         cr = self.env.cr
