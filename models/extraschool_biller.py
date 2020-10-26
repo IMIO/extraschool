3416b04e46985b678eafe898e8997af4becac215
jpans <jenny.pans@imio.be>
2020-10-26 16:45:10 +0100

    fix bug with filter in mail invoice

--- models/extraschool_biller.py
+++ models/extraschool_biller.py
@@ -192,8 +192,8 @@ class extraschool_biller(models.Model):
                 'target': 'current',
                 'limit': 50000,
                 'domain': [('biller_id.id', '=', self.id),
-                           ('balance', '>', 0),
-                           '|', ('invoicesendmethod', '=', 'onlybymail'), ('invoicesendmethod', '=', 'emailandmail')]
+                           '|', ('invoicesendmethod', '=', 'onlybymail'), ('invoicesendmethod', '=', 'emailandmail')],
+                'context': {"search_default_actif": 1},
                 }
 
     @api.multi
